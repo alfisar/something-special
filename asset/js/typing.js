5e2a1051c582bf59b713232fabaca725455b2c64
alfisar <alfisar589@gmail.com>
2022-12-12 19:06:48 +0700

    finish

---
+++ asset/js/typing.js
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+var typing=new Typed(".text", {
+    Strings : ["hallo", "Kamu"],
+    typeSpeed:100,
+    backSpeed:40,
+    loop:true
+})
